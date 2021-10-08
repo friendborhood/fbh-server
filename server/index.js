49ef01b712e793e7988320c2e04baea782752100
Omer Gery <omergery1996@gmail.com>
2021-10-08 13:12:19 +0300

    started

--- server/index.js
+++ server/index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-let dataMap = new Map();
-dataMap.set('omer',4);
+const cors = require('cors')
+app.use(cors());
 
+
+
+let dataMap = new Map();
+dataMap.set('omer','ahla');
+dataMap.set('daniel','panda');
+dataMap.set('arzi','manga');
+
+
+// will be in use later
 app.post('/endPoint', (req, res) => {
     console.log(req.body);
   dataMap.set(req.body.key,req.body.value);
@@ -13,17 +22,18 @@ app.post('/endPoint', (req, res) => {
   });
 });
 
-app.get('/endPoint', (req, res) => {
-    //console.log(req);
+app.get('/endPoint/:index', (req, res) => {
+  console.log("REQUEST GET");
+    const key = req.params.index;
+    console.log(key);
   res.json({
-    status:'200',
-    value:dataMap.get(req.query.key),
+    value:dataMap.get(key),
   });
 });
 
-  const server = app.listen(3001);
-  console.log('listeneing');
+  const PORT_NUMBER = 3001;
+  const server = app.listen(PORT_NUMBER);
+  console.log(`the server has started...
+   port: ${PORT_NUMBER}`);
   
-  
-
 module.exports = server;
