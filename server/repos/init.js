9c533b2e0a7bb5a3c84e952997372dd10fda83f2
Omer Gery <68545675+OmerGery@users.noreply.github.com>
2021-12-30 21:31:16 +0200

    Update init.js

--- server/repos/init.js
+++ server/repos/init.js
@@ -5,6 +5,6 @@ const FIRE_BASE_CONFIG = {
   apiKey: 'AIzaSyCegQ_467ySkRnCfsR_sDgpeb_2akjmTV0', authDomain: 'firendborhood.firebaseapp.com', databaseURL: 'https://firendborhood-default-rtdb.europe-west1.firebasedatabase.app', projectId: 'firendborhood', storageBucket: 'firendborhood.appspot.com', messagingSenderId: '354847400901', appId: '1:354847400901:web:d413ea1df4d2b28eb521ac',
 };
 
-const firebaseConfig = JSON.parse(FIRE_BASE_CONFIG);
+const firebaseConfig = FIRE_BASE_CONFIG;
 const app = initializeApp(firebaseConfig);
 module.exports = app;
