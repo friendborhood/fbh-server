82fc4fb5692976963df7c604bf75b384c83431de
Omer Gery <68545675+OmerGery@users.noreply.github.com>
2021-12-31 12:26:50 +0200

    wip

--- server/repos/init.js
+++ server/repos/init.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { initializeApp } = require('firebase/app');
 
 console.log(process.env.FIRE_BASE);
-const FIRE_BASE_CONFIG = process.env.FIRE_BASE;
+const FIRE_BASE_CONFIG = JSON.parse(process.env.FIRE_BASE);
 console.log(FIRE_BASE_CONFIG);
 const firebaseConfig = FIRE_BASE_CONFIG;
 const app = initializeApp(firebaseConfig);
