ef734b91814fd677b5712a80a3a1c43eef03e62d
Omer Gery <68545675+OmerGery@users.noreply.github.com>
2021-12-31 10:51:27 +0200

    Update init.js

--- server/repos/init.js
+++ server/repos/init.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const { initializeApp } = require('firebase/app');
 
-const FIRE_BASE_CONFIG = {
-  apiKey: 'AIzaSyCegQ_467ySkRnCfsR_sDgpeb_2akjmTV0', authDomain: 'firendborhood.firebaseapp.com', databaseURL: 'https://firendborhood-default-rtdb.europe-west1.firebasedatabase.app', projectId: 'firendborhood', storageBucket: 'firendborhood.appspot.com', messagingSenderId: '354847400901', appId: '1:354847400901:web:d413ea1df4d2b28eb521ac',
-};
-
+const FIRE_BASE_CONFIG = JSON.parse(process.env.FIRE_BASE);
+console.log(FIRE_BASE_CONFIG);
 const firebaseConfig = FIRE_BASE_CONFIG;
 const app = initializeApp(firebaseConfig);
 module.exports = app;
