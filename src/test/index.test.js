e2112c3ae077ba69bf224632f70cd924941b7e40
Omer Gery <omergery1996@gmail.com>
2022-01-16 00:55:11 +0200

    Update index.test.js

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -7,7 +7,7 @@ describe('Basic sanity server CRUD tests', () => {
   it('Can get user', async () => {
     const { data, status } = await axios.get('http://localhost:3000/users/1');
     expect(data).toBeDefined();
-    expect(status).toBe(200);
+    expect(status).toBe(201);
   });
   it('Return 404 for not exisiting user', async () => {
     try {
