afdcc4248689dc5c649bad70ef66b05d23398a12
Omer Gery <omergery1996@gmail.com>
2022-03-16 11:00:48 +0200

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const swaggerUi = require('swagger-ui-express'),
-swaggerDocument = require('../../swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 
 const app = express();
 app.use(express.json());
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(
   '/api-docs',
-  swaggerUi.serve, 
-  swaggerUi.setup(swaggerDocument)
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument),
 );
 app.get('/', (req, res) => {
   console.log('GET');
