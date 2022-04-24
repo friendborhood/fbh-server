3c84a0fa05e6b8b94b048a6d043439164426892b
dolev7 <d.dolev7@gmail.com>
2022-04-24 13:15:37 +0300

    test for github jira integration

---
+++ src/api/offer/index.js
@@ -0,0 +1,99 @@
+/* eslint-disable consistent-return */
+const { Router } = require('express');
+const {
+  addItem,
+  findByCategory, findById,
+  findByName,
+  findAll,
+  getAllCategories,
+  validateItemData,
+  deleteItem,
+  patchItem,
+} = require('../../models/item');
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+  console.log('try get all items');
+  const { categoryName } = req.query;
+  const items = categoryName ? await findByCategory(categoryName) : await findAll();
+  if (!items) {
+    return res.status(404).json({ msg: 'Items were not found.' });
+  }
+  return res.json(items);
+});
+router.get('/:itemId', async (req, res) => {
+  console.log('try get item');
+  const { itemId } = req.params;
+  console.log(`item id: ${itemId}`);
+  const item = await findById(itemId);
+  if (!item) {
+    return res.status(404).json({ msg: `Item with id ${itemId} was not found.` });
+  }
+  return res.json(item);
+});
+router.get('/categories', async (req, res) => {
+  const categories = await getAllCategories();
+  console.log(categories);
+  return res.json({ categories });
+});
+router.post('/', async (req, res) => {
+  try {
+    const data = req.body;
+    try {
+      await validateItemData(data);
+    } catch (e) {
+      return res.status(400).json({ error: e.message });
+    }
+    console.log(`try add item by name ${data.itemName}`);
+    const isExist = await findByName(data.itemName);
+    if (isExist) {
+      return res.status(400).json({ msg: `Item name ${data.itemName} already exists. item name must be unique` });
+    }
+    console.log(`try add item with data ${JSON.stringify(data)}`);
+    const newItemId = await addItem(data);
+
+    return res.json({
+      msg: 'item was added to database successfully',
+      itemId: newItemId,
+    });
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ error: e.message });
+  }
+});
+
+router.delete('/:itemId', async (req, res) => {
+  console.log('try get item');
+  const { itemId } = req.params;
+  console.log(`item id: ${itemId}`);
+  const item = await findById(itemId);
+  if (!item) {
+    return res.status(404).json({ msg: `Item with id ${itemId} was not found.` });
+  }
+  await deleteItem(itemId);
+  return res.status(200).json({ msg: `item with id:${itemId} was deleted` });
+});
+
+router.patch('/:itemId', async (req, res) => {
+  try {
+    console.log('try get item');
+    const { itemId } = req.params;
+    console.log(`item id: ${itemId}`);
+    const item = await findById(itemId);
+    if (!item) {
+      return res.status(404).json({ msg: `Item with id ${itemId} was not found.` });
+    }
+    const data = req.body;
+    await patchItem(data, itemId);
+    return res.json({
+      msg: 'item was updated in database successfully',
+      itemId,
+    });
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ error: e.message });
+  }
+});
+
+module.exports = router;
---
+++ src/models/offer/index.js
@@ -0,0 +1,97 @@
+const Joi = require('joi');
+const { uuid } = require('short-uuid');
+const getModel = require('../../services/firebase-api/get');
+const upsert = require('../../services/firebase-api/upsert');
+const add = require('../../services/firebase-api/add');
+
+const modelName = 'items';
+const getAllCategories = async () => {
+  const { categories } = await getModel(modelName);
+  return categories;
+};
+
+const validateItemData = async (data) => {
+  console.log('validating item data : ', data);
+  const categories = await getAllCategories();
+  console.log(categories);
+  const schema = Joi.object({
+    itemName: Joi.string()
+      .min(3)
+      .max(30)
+      .required(),
+    categoryName: Joi.string().valid(...categories),
+    priceRange: Joi.any().required(),
+    imageUrl: Joi.string().uri(),
+  });
+  await schema.validateAsync(data);
+  console.log('item data is okay');
+};
+const findAll = async () => {
+  console.log('getting model from db');
+  const itemModel = await getModel(modelName);
+  return itemModel;
+};
+
+const findByName = async (itemName) => {
+  const itemModel = await getModel(modelName);
+  console.log(`try find item ${itemName}`);
+  const relevantItem = Object.values(itemModel)
+    .find((item) => item.itemName === itemName);
+
+  if (!relevantItem) {
+    console.log(`item ${itemName} was not found`);
+    return null;
+  }
+  console.log(`item ${itemName} was  found `);
+
+  return relevantItem;
+};
+const findById = async (index) => {
+  console.log('getting model from db');
+  const itemModel = await getModel(modelName);
+  const relevantItem = itemModel[index];
+  if (!relevantItem) {
+    console.log('item was not found');
+    return null;
+  }
+  return relevantItem;
+};
+const findByCategory = async (categoryName) => {
+  const itemModel = await getModel(modelName);
+  console.log(`try to find items in ${categoryName}`);
+  console.log(itemModel);
+  const relevantItems = Object.values(itemModel)
+    .filter((item) => item.categoryName === categoryName);
+  if (!relevantItems) {
+    console.log(`items in ${categoryName} were not found`);
+    return null;
+  }
+  console.log(`item in ${categoryName} were found `);
+
+  return relevantItems;
+};
+const addItem = async (data) => {
+  console.log('adding item to db');
+  const generatedId = uuid();
+  await upsert(modelName, data, generatedId);
+  return generatedId;
+};
+const deleteItem = async (index) => {
+  console.log('deleting item from db');
+  await add(modelName, null, index);
+};
+const patchItem = async (data, itemId) => {
+  console.log(`patching item ${itemId}, modifing data ${JSON.stringify(data)}`);
+  await upsert(modelName, data, itemId);
+};
+module.exports = {
+  addItem,
+  findByCategory,
+  findById,
+  findByName,
+  findAll,
+  validateItemData,
+  getAllCategories,
+  deleteItem,
+  patchItem,
+};
