e17340c002d7767ed0114b420c9240a7229ead51
Omer Gery <omergery1996@gmail.com>
2022-04-25 20:14:05 +0300

    Update index.js

--- src/models/offer/index.js
+++ src/models/offer/index.js
@@ -7,43 +7,45 @@ const getOffersInArea = ({ offers, radiusInMeters, targetLocation }) => {
   });
   return offersInRadius;
 };
-
-const relevantOffers = getOffersInArea({
-  offers: {
-    offerBeerShave: {
-      name: 'beer sheva',
-      location: {
-        geoCode: {
-          lat: 31.27566902435525, lon: 34.75106102314429,
-        },
-      },
-    },
-    offerTlv: {
-      name: 'shooki kosem',
-      location: {
-        geoCode: {
-          lat: 32.1716207368198, lon: 34.847595734248976,
-        },
-      },
-    },
-    offerUsa: {
-      location: {
-        geoCode: {
-          lat: 38.68283263477214, lon: -9.063595043382376,
-        },
-      },
-    },
-    offerAfrica: {
-      location: {
-        geoCode: {
-          lat: 28.19485862635811, lon: 18.348766841649137,
-        },
-      },
-    },
-  },
-  radiusInMeters: 20000,
-  targetLocation: {
-    lat: 32.06235266605632, lon: 34.77191901881545,
-  },
-});
-console.log(JSON.stringify(relevantOffers));
+module.exports = { getOffersInArea };
+// const relevantOffers = getOffersInArea({
+//   offers: {
+//     offerBeerShave: {
+//       name: 'beer sheva',
+//       location: {
+//         geoCode: {
+//           lat: 31.27566902435525, lon: 34.75106102314429,
+//         },
+//       },
+//     },
+//     offerTlv: {
+//       name: 'shooki kosem',
+//       location: {
+//         geoCode: {
+//           lat: 32.1716207368198, lon: 34.847595734248976,
+//         },
+//       },
+//     },
+//     offerUsa: {
+//       name: 'yuval',
+//       location: {
+//         geoCode: {
+//           lat: 38.68283263477214, lon: -9.063595043382376,
+//         },
+//       },
+//     },
+//     offerAfrica: {
+//       name: 'eden',
+//       location: {
+//         geoCode: {
+//           lat: 28.19485862635811, lon: 18.348766841649137,
+//         },
+//       },
+//     },
+//   },
+//   radiusInMeters: 20000000000000,
+//   targetLocation: {
+//     lat: 32.06235266605632, lon: 34.77191901881545,
+//   },
+// });
+// console.log(JSON.stringify(relevantOffers));
