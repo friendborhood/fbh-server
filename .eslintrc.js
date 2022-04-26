c46756d7d2d77abad8493fcefc4775d4f2943a1f
dolev7 <d.dolev7@gmail.com>
2022-04-26 20:50:28 +0300

    BACK-2 linter fix?

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = 
+{
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+    'guard-for-in': 0,
+    'no-await-in-loop': 0,
+    'no-restricted-syntax': 0,
+    'no-return-assign': 0,
+    'no-dupe-class-members': 0,
+    'import/prefer-default-export': 0,
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  ignorePatterns: ['lib/*', 'node_modules/', 'jest.config.js'],
+  settings: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
