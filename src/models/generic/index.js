1fb8ba23d05725e058f8d16b6ca080c38d74b67a
Omer Gery <omergery1996@gmail.com>
2022-04-28 20:35:45 +0300

    Update index.js

--- src/models/generic/index.js
+++ src/models/generic/index.js
@@ -3,11 +3,10 @@ const { uuid } = require('short-uuid');
 const add = require('../../services/firebase-api/add');
 const upsert = require('../../services/firebase-api/upsert');
 
-const addTimeStamp = (data) => ({ ...data, lastUpdatedAt: `${moment.tz('Asia/Jerusalem')}` });
+const addTimeStamp = (data) => ({ ...data, lastUpdatedAt: `${moment.tz('Asia/Jerusalem').format()}` });
 const addUuidEntity = async ({ data, modelName }) => {
   const generatedId = uuid();
   const timeStampedData = addTimeStamp(data);
-  console.log(timeStampedData);
   await upsert(modelName, timeStampedData, generatedId);
   return generatedId;
 };
