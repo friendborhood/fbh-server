8135943a7381fa7bfbb62dd8ea32716bcf1d1d20
Omer Gery <omergery1996@gmail.com>
2022-09-03 19:11:41 +0300

    Update index.test.ts

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -42,7 +42,7 @@ describe('Basic sanity server CRUD tests', () => {
     });
     await Promise.all(offers.map((offer) => validateOfferData(offer)));
     expect(offers[0].distanceFromUser).toBeLessThanOrEqual(offers[1].distanceFromUser);
-    expect(offers.length).toBe(3);
+    expect(offers.length).toBe(5);
   });
   it('Offers in area - Mock', () => {
     const relevantOffers = filterOffersByArea(
