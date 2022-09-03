047d16789c50ee32d27f4f1a3b032ab5e871aef6
Daniel Dolev <d.dolev7@gmail.com>
2022-09-03 19:14:18 +0300

    sort items by itemName + linter fixes

--- src/api/item/index.ts
+++ src/api/item/index.ts
@@ -1,35 +1,36 @@
 /* eslint-disable import/no-import-module-exports */
 /* eslint-disable consistent-return */
-import Router from "express";
-import { adminMiddleWare } from "../../auth";
+import Router from 'express';
+import { adminMiddleWare } from '../../auth';
 import {
   findByCategory,
   findById,
   findByName,
   findAll,
   validateItemData,
-} from "../../models/item";
+} from '../../models/item';
 
-import { addUuidEntity, deleteEntity, patchEntity } from "../../models/generic";
-import logger from "../../logger";
+import { addUuidEntity, deleteEntity, patchEntity } from '../../models/generic';
+import logger from '../../logger';
 
-const ITEM_MODEL = "items";
+const ITEM_MODEL = 'items';
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  logger.info("try get all items");
+router.get('/', async (req, res) => {
+  logger.info('try get all items');
   const { categoryName } = req.query;
   const items = categoryName
     ? await findByCategory(categoryName)
     : await findAll();
   if (items.length === 0) {
-    return res.status(204).json({ msg: "Items were not found." });
+    return res.status(204).json({ msg: 'Items were not found.' });
   }
-  return res.json(items);
+  const sortedItems = items.sort((a, b) => a.itemName.localeCompare(b.itemName));
+  return res.json(sortedItems);
 });
-router.get("/:itemId", async (req, res) => {
-  logger.info("try get item");
+router.get('/:itemId', async (req, res) => {
+  logger.info('try get item');
   const { itemId } = req.params;
   logger.info(`item id: ${itemId}`);
   const item = await findById(itemId);
@@ -41,7 +42,7 @@ router.get("/:itemId", async (req, res) => {
   return res.json(item);
 });
 
-router.post("/", adminMiddleWare, async (req, res) => {
+router.post('/', adminMiddleWare, async (req, res) => {
   try {
     const data = req.body;
     try {
@@ -60,7 +61,7 @@ router.post("/", adminMiddleWare, async (req, res) => {
     const newItemId = await addUuidEntity({ data, modelName: ITEM_MODEL });
 
     return res.json({
-      msg: "item was added to database successfully",
+      msg: 'item was added to database successfully',
       itemId: newItemId,
     });
   } catch (e) {
@@ -69,8 +70,8 @@ router.post("/", adminMiddleWare, async (req, res) => {
   }
 });
 
-router.delete("/:itemId", adminMiddleWare, async (req, res) => {
-  logger.info("try get item");
+router.delete('/:itemId', adminMiddleWare, async (req, res) => {
+  logger.info('try get item');
   const { itemId } = req.params;
   logger.info(`item id: ${itemId}`);
   const item = await findById(itemId);
@@ -83,9 +84,9 @@ router.delete("/:itemId", adminMiddleWare, async (req, res) => {
   return res.status(200).json({ msg: `item with id:${itemId} was deleted` });
 });
 
-router.patch("/:itemId", adminMiddleWare, async (req, res) => {
+router.patch('/:itemId', adminMiddleWare, async (req, res) => {
   try {
-    logger.info("try get item");
+    logger.info('try get item');
     const { itemId } = req.params;
     logger.info(`item id: ${itemId}`);
     const item = await findById(itemId);
@@ -97,7 +98,7 @@ router.patch("/:itemId", adminMiddleWare, async (req, res) => {
     const data = req.body;
     await patchEntity({ data, modelName: ITEM_MODEL, entityId: itemId });
     return res.json({
-      msg: "item was updated in database successfully",
+      msg: 'item was updated in database successfully',
       itemId,
     });
   } catch (e) {
